f1d66a200c1f24accd7a20644d803dd8c29f13a7
NetaVizenberg <155624389+NetaVizenberg@users.noreply.github.com>
2026-08-19 11:17:25 +0300

    fix: output both EC2 instances

--- outputs.tf
+++ outputs.tf
@@ -8,17 +8,17 @@ output "public_subnet_id" {
   value       = module.vpc.public_subnet_id
 }
 
-output "private_subnet_id" {
-  description = "ID of the private subnet."
-  value       = module.vpc.private_subnet_id
+output "security_group_id" {
+  description = "ID of the EC2 security group."
+  value       = module.vpc.security_group_id
 }
 
-output "ec2_instance_id" {
-  description = "ID of the EC2 instance."
-  value       = module.ec2.instance_id
+output "ec2_instance_ids" {
+  description = "IDs of the two EC2 instances."
+  value       = [module.ec2_a.instance_id, module.ec2_b.instance_id]
 }
 
-output "ec2_private_ip" {
-  description = "Private IP address of the EC2 instance."
-  value       = module.ec2.private_ip
+output "ec2_private_ips" {
+  description = "Private IP addresses of the two EC2 instances."
+  value       = [module.ec2_a.private_ip, module.ec2_b.private_ip]
 }
