65f5bc3c752f7ddc91547dbef86dd96c1b492741
NetaVizenberg <155624389+NetaVizenberg@users.noreply.github.com>
2026-08-19 11:08:47 +0300

    feat: add VPC module variables

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  description = "Name used to tag VPC resources."
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC."
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet."
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet."
+  type        = string
+}
