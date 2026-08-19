186bd5f2e89e12ec0b95961b15c8b2d34cfae6c4
NetaVizenberg <155624389+NetaVizenberg@users.noreply.github.com>
2026-08-19 11:16:43 +0300

    fix: output VPC security group ID

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -8,7 +8,7 @@ output "public_subnet_id" {
   value       = aws_subnet.public.id
 }
 
-output "private_subnet_id" {
-  description = "ID of the private subnet."
-  value       = aws_subnet.private.id
+output "security_group_id" {
+  description = "ID of the SSM-only EC2 security group."
+  value       = aws_security_group.ec2.id
 }
