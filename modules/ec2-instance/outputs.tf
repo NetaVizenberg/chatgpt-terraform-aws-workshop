974f0da286b0991be4d4e030b1165a9e800a8f4e
NetaVizenberg <155624389+NetaVizenberg@users.noreply.github.com>
2026-08-19 11:22:32 +0300

    fix: remove stale security group output from EC2 module

--- modules/ec2-instance/outputs.tf
+++ modules/ec2-instance/outputs.tf
@@ -7,8 +7,3 @@ output "private_ip" {
   description = "Private IP address of the EC2 instance."
   value       = aws_instance.this.private_ip
 }
-
-output "security_group_id" {
-  description = "ID of the EC2 security group."
-  value       = aws_security_group.this.id
-}
